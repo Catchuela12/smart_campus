33fbb821788da7ba7a94ae59dc0034820db079e7
Jazrel <constantinojazrel@gmail.com>
2026-03-08 20:56:59 +0800

    Change and change

--- app/src/main/java/com/example/smart_campus/screen/Announcement_data/AnnouncementAppDataBase.kt
+++ app/src/main/java/com/example/smart_campus/screen/Announcement_data/AnnouncementAppDataBase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Announcement::class], version = 1, exportSchema = false)
+@Database(entities = [Announcement::class], version = 2, exportSchema = false)
 abstract class AnnouncementAppDataBase : RoomDatabase() {
 
     abstract fun announcementDao(): AnnouncementDao
@@ -20,7 +20,10 @@ abstract class AnnouncementAppDataBase : RoomDatabase() {
                     context.applicationContext,
                     AnnouncementAppDataBase::class.java,
                     "announcement_database"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration() // Simplest for development when schema changes
+                    .build()
+
                 INSTANCE = instance
                 instance
             }
